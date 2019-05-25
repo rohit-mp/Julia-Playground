8de64193c9796ffa38c6639132ff3840a37543d8
Rohit M P <rohitmp99@gmail.com>
2019-05-25 14:51:09 +0530

    Removed unnecessary comments

--- arrayAddition/arrayAddition.jl
+++ arrayAddition/arrayAddition.jl
@@ -16,17 +16,10 @@ function arrayAdd!(a, b, c)
 end
 
 function bench1!(d_a, d_b, d_c)
-    @sync begin #not sure why I'm using this
+    @sync begin
         @cuda blocks=ceil(Int, length(d_a)/1024) threads=1024 arrayAdd!(d_a, d_b, d_c)
     end
 end
-#=The most interesting part of this is the call to CuArrays.@sync.
-The CPU can assign jobs to the GPU and then go do other stuff (such as assigning more jobs to the GPU) while 
-the GPU completes its tasks. Wrapping the execution in a CuArrays.@sync block will make the CPU block until the 
-queued GPU tasks are done, similar to how Base.@sync waits for distributed CPU tasks.
-Without such a synchronization, you'd be measuring the time takes to launch the computation, 
-not the time to perform the computation. But most of the time you don't need to synchronize explicitly: 
-many operations, like copying memory from the GPU to the CPU, implicitly synchronize execution.=#
 
 function bench2!(d_a, d_b, d_c)
     d_c .= d_b .+ d_a
@@ -42,7 +35,6 @@ d_b = CuArray(h_b)
 d_c = similar(d_a)
 
 bench1!(d_a, d_b, d_c)
-#synchronize() if you're printing anything in the kernel
 
 h_c = Array(d_c)
 
